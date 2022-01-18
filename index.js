60dd8478d75899edf74d7fd9553934100237cef1
lazylion2 <lazylion2@gmail.com>
2022-01-18 13:23:01 +0200

    added dates with hour

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var dates = [];
-var count = 1;
+var backupDates =[];
+var count = 0;
 function main(){
     var storedNames = JSON.parse(localStorage.getItem("dates"));
     if (!storedNames){
         console.log('no dates in storage')
     }
     else{
-        var storedNames = JSON.parse(localStorage.getItem("names"));
+        var dates = JSON.parse(localStorage.getItem("dates"));
+        var backupDates = JSON.parse(localStorage.getItem("dates"));
+
     }
 }
 
 function addDate(){
-    console.log('addDates')
+    // console.log('addDates')
     var table = document.getElementById('table')
     var newRow = table.insertRow()
-    newRow.insertCell().appendChild(document.createTextNode(count%8));
-    newRow.insertCell().appendChild(document.createTextNode(new Date().toLocaleDateString()));
-    if (count%8==0){
+    const tempDate =new Date().toLocaleDateString()
+    const tempHour = new Date().toLocaleTimeString()
+    const newDate = `${tempDate} ${tempHour}`
+    newRow.insertCell().appendChild(document.createTextNode((count%8)+1));
+    newRow.insertCell().appendChild(document.createTextNode(newDate));
+    if (count%8==7){
         var emptyRow = table.insertRow()
 
         emptyRow.insertCell().appendChild(document.createTextNode(''));
@@ -26,6 +32,9 @@ function addDate(){
     count ++
      
 }
+function removeDate(){
+}
+
 var btnAdd = document.getElementById('btnAdd')
 btnAdd.addEventListener("click", e =>{
     addDate()
