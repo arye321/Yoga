62922b0159dcec57f4727006db2bdf6fab6c7436
lazylion2 <lazylion2@gmail.com>
2022-01-18 20:18:00 +0200

    all working i think

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var dates = [];
 var backupDates =[];
-var count = 0;
+var table = document.getElementById('table')
+
 function main(){
-    var storedNames = JSON.parse(localStorage.getItem("dates"));
-    if (!storedNames){
+    var storedDates = JSON.parse(localStorage.getItem("dates3"));
+    if (!storedDates){
         console.log('no dates in storage')
     }
     else{
-        var dates = JSON.parse(localStorage.getItem("dates"));
-        var backupDates = JSON.parse(localStorage.getItem("dates"));
+        dates = JSON.parse(localStorage.getItem("dates3"));
+        backupDates = JSON.parse(localStorage.getItem("dates3"));
         for (const date of dates){
             addDate(date)
            
@@ -22,36 +23,70 @@ function addDate(newDate){
         const tempDate =new Date().toLocaleDateString()
         const tempHour = new Date().toLocaleTimeString()
         newDate = `${tempDate} ${tempHour}`
+        dates.push(newDate)
+        backupDates.push(newDate)
     }
+    var rowCount = table.rows.length;
 
     // console.log('addDates')
-    var table = document.getElementById('table')
     var newRow = table.insertRow()
     
-    newRow.insertCell().appendChild(document.createTextNode((count%8)+1));
+    newRow.insertCell().appendChild(document.createTextNode((rowCount%9)));
     newRow.insertCell().appendChild(document.createTextNode(newDate));
-    if (count%8==7){
+    rowCount = table.rows.length;
+    
+    if (rowCount%9==0){
         var emptyRow = table.insertRow()
 
         emptyRow.insertCell().appendChild(document.createTextNode(''));
         emptyRow.insertCell().appendChild(document.createTextNode(''));
 
     }
-    dates.push(newDate)
-    backupDates.push(newDate)
 
-    localStorage.setItem("dates", JSON.stringify(dates));
-    count ++
+    localStorage.setItem("dates3", JSON.stringify(dates));
+
      
 }
 function removeDate(){
+    var rowCount = table.rows.length;
+    
+    table.deleteRow(rowCount -1);
+    dates.splice(-1)
+    localStorage.setItem("dates3", JSON.stringify(dates));
+    if ( table.rows[rowCount-2].cells[1].textContent == '' ){
+        table.deleteRow(rowCount -2);
+    }
+
 }
+function killDates(){
+    dates = []
+    localStorage.setItem("dates3", JSON.stringify(dates));
 
 
+    table.innerHTML =`<tr > <th>מספר</th> <th>תאריך</th> </tr>`
+
+}
+function undoDates(){
+    table.innerHTML =`<tr > <th>מספר</th> <th>תאריך</th> </tr>`
+
+    dates = backupDates
+    localStorage.setItem("dates3", JSON.stringify(backupDates));
+    
+    for (const date of backupDates){
+         addDate(date)
+    }
+    
+}
 document.getElementById('btnAdd').addEventListener("click", e =>{
     addDate()
 }); 
+document.getElementById('btnRemove').addEventListener("click", e =>{
+    removeDate()
+}); 
 document.getElementById('btnKillALL').addEventListener("click", e =>{
-    addDatefrom()
+    killDates()
+}); 
+document.getElementById('btnUndo').addEventListener("click", e =>{
+    undoDates()
 }); 
 main()
