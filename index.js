5f4f8513e263bda79d4a0f44e6fee073bdb201f9
lazylion2 <lazylion2@gmail.com>
2022-01-18 13:35:26 +0200

    storage add working

--- index.js
+++ index.js
@@ -9,17 +9,25 @@ function main(){
     else{
         var dates = JSON.parse(localStorage.getItem("dates"));
         var backupDates = JSON.parse(localStorage.getItem("dates"));
+        for (const date of dates){
+            addDate(date)
+           
+        }
 
     }
 }
 
-function addDate(){
+function addDate(newDate){
+    if(!newDate){
+        const tempDate =new Date().toLocaleDateString()
+        const tempHour = new Date().toLocaleTimeString()
+        newDate = `${tempDate} ${tempHour}`
+    }
+
     // console.log('addDates')
     var table = document.getElementById('table')
     var newRow = table.insertRow()
-    const tempDate =new Date().toLocaleDateString()
-    const tempHour = new Date().toLocaleTimeString()
-    const newDate = `${tempDate} ${tempHour}`
+    
     newRow.insertCell().appendChild(document.createTextNode((count%8)+1));
     newRow.insertCell().appendChild(document.createTextNode(newDate));
     if (count%8==7){
@@ -29,14 +37,21 @@ function addDate(){
         emptyRow.insertCell().appendChild(document.createTextNode(''));
 
     }
+    dates.push(newDate)
+    backupDates.push(newDate)
+
+    localStorage.setItem("dates", JSON.stringify(dates));
     count ++
      
 }
 function removeDate(){
 }
 
-var btnAdd = document.getElementById('btnAdd')
-btnAdd.addEventListener("click", e =>{
+
+document.getElementById('btnAdd').addEventListener("click", e =>{
     addDate()
+}); 
+document.getElementById('btnKillALL').addEventListener("click", e =>{
+    addDatefrom()
 }); 
 main()
